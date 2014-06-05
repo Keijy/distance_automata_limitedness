e87c4860ed1c2782df076204bf7a013446c2050d
Juedong Xue <jxue@emi.u-bordeaux1.fr>
2014-06-05 21:22:53 +0200

    Matrice.c

--- Matrice.c
+++ Matrice.c
@@ -9,23 +9,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h> 
+#include <string.h>
 
 #include <math.h>
 
-#define FALSE 1
-#define TRUE 0
-#define MAXMOT 10
+
 struct _Matrice {
   int** tab;
-  char mot[MAXMOT];
+  char* mot;
   int taille;
 };
 
 Matrice creer_matrice(char* mot, int taille){
-  printf("nom matrice : %s\n", mot);
   Matrice m = malloc(sizeof(*m));
   m->tab = malloc(taille * sizeof(int*));
-  strcpy(m->mot, mot);
+  m->mot = mot;
   m->taille = taille;
 
   int i;
@@ -46,19 +44,9 @@ void detruire_matrice(Matrice m){
   free(m);  
 }
 
-int get_taille(Matrice m){
-  return m->taille;
-}
-
-char* get_mot(Matrice m){
-  return m->mot;
-}
-
 Matrice creer_matrice_transistions(Automate* a, char l){
-  a = creer_automate_etats_0_n(a);
   int n = taille_ensemble(get_etats(a));
   Matrice m = creer_matrice(&l, n);
-
   Table_iterateur it1;
   Ensemble_iterateur it2;
   for ( it1 = premier_iterateur_table(get_transitions(a));
@@ -68,32 +56,29 @@ Matrice creer_matrice_transistions(Automate* a, char l){
     Cle * cle = (Cle*) get_cle(it1);
     Ensemble * fins = (Ensemble*) get_valeur(it1);
     int tmp = get_lettre_cle(cle);
-  
     if(tmp == (int)l){
       for( it2 = premier_iterateur_ensemble(fins);
 	     ! iterateur_ensemble_est_vide(it2);
 	     it2 = iterateur_suivant_ensemble(it2)
 	     ){
 	    int f = get_element(it2);
-	    m->tab[get_origine_cle(cle)][f] = get_cout_cle(cle);
+	    m->tab[get_origine_cle(cle)-1][f-1] = get_cout_cle(cle);
       }
     }
   }
-  liberer_automate(a);
   return m;
 }
 
 void print_matrice(Matrice m){
-  printf("M(%s) :\n\t", m->mot);
   int i, j;
   for(i = 0; i<m->taille; i++){
     printf("|");
     for(j = 0; j<m->taille; j++){
       printf(" %d ", m->tab[i][j]);
     }
-    printf("|\n\t");
+    printf("|\n");
   }
-    printf("\n");
+  printf("\n");
 }
 
 int min(int n1,int n2){
@@ -101,26 +86,20 @@ int min(int n1,int n2){
     return n1;
   return n2;
 }
-
 int max(int n1,int n2){
   if(n1>n2)
     return n1;
   return n2;
 }
-
 int equal(Matrice m1,Matrice m2){
   return m1->taille==m2->taille;
 }
-
 Matrice multiplication(Matrice m1,Matrice m2){
   if(!equal(m1,m2)){
     printf("sont pas de meme taille");
     return NULL;
   }
-  
-  char c[MAXMOT];
-  strcpy(c, m1->mot);
-  strcat(c, m2->mot);
+  char* c=strcat(m1->mot,m2->mot);  
   Matrice m3=creer_matrice(c,m1->taille);
   int i,j,k;
   for(i=0;i<m1->taille;i++)
@@ -128,8 +107,8 @@ Matrice multiplication(Matrice m1,Matrice m2){
       m3->tab[i][j]=-1;
     }  
   for(i=0;i<m1->taille;i++)
-    for(j=0;j<m1->taille;j++)   
-   for(k=0;k<m1->taille;k++){
+    for(j=0;j<m1->taille;j++)
+      for(k=0;k<m1->taille;k++){
 	if(m3->tab[i][j] == -1)
 	  m3->tab[i][j]= m1->tab[i][k]+m2->tab[k][j];
 	else
@@ -138,32 +117,19 @@ Matrice multiplication(Matrice m1,Matrice m2){
   return m3;
 }
 
-/* Condition 1 : pour tout i,j,k  e(i,j) <= max(e(i,k), e(k,j))
- * Condition 2 : pour tout i,j, il existe k tq e(i,j) = max(e(i,k), e(k,j))
- * Condition 1 && Condition 2 <=> m est idempotent
- */
 
-int est_idempotent(Matrice m){
-  int cond1 = 1;
-  int cond2 = 0;
-
-  int i,j,k;
-  for(i = 0; i < m->taille; i++){
-    for(j = 0; j < m->taille; j++){
-      for(k = 0; k < m->taille; k++){
-	if(m->tab[i][j] > max(m->tab[i][k], m->tab[k][j]))
-	  cond1 = 0;
-	if(m->tab[i][j] == max(m->tab[i][k], m->tab[k][j]))
-	  cond2 = 1;
-      }
-      if(!(cond1 && cond2)) {
-	return FALSE;
-      }
-      else{
-	cond1 = 1; 
-	cond2 = 0;
-      }
-    }
+int anchored(int i,int j,Matrice m){
+  int k;
+  for(k=1;k<m->taille;k++){
+    if(m->tab[k][k]==0 && m->tab[k][k] <= m->tab[i][j] && m->tab[i][j] == max(m->tab[i][k],m->tab[k][j]))
+      return 1;
   }
-  return TRUE;
+  return 0;
 }
+  
+int stable(int i,int j,Matrice m){
+  return m->tab[i][j]==1 && anchored(i,j,m)==0;
+}
+    
+
+
